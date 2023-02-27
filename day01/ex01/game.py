27ddfa167f8a8aaea2e5050ce32bb0f947f165b9
Houssam-eddine Moumanis <houssam.moumanis@gmail.com>
2023-02-28 00:35:11 +0100

    feat - adapting day01/ex01

--- day01/ex01/game.py
+++ day01/ex01/game.py
@@ -1,12 +1,13 @@
 class GotCharacter:
-    def __init__(self, first_name, is_alive=True):
+    """A class representing a Game of Thrones character."""
+    def __init__(self, first_name=None, is_alive=True):
         self.first_name = first_name
         self.is_alive = is_alive
 
 class Stark(GotCharacter):
     """A class representing the Stark family. Or when bad things happen to good people."""
-    def __init__(self, first_name, is_alive=True):
-        super().__init__(first_name, is_alive)
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Stark"
         self.house_words = "Winter is Coming"
     def print_house_words(self):
@@ -16,8 +17,8 @@ class Stark(GotCharacter):
 
 class Targaryan(GotCharacter):
     """A class representing the Targaryan family. Or a Barbecue restaurant :v."""
-    def __init__(self, first_name, is_alive=True):
-        super().__init__(first_name, is_alive)
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Targaryan"
         self.house_words = "Fire and Blood"
     def print_house_words(self):
