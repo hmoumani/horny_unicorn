6a2deb8ad3e26b58ce3c75d0185f190dd02e921e
Houssam-eddine Moumanis <houssam.moumanis@gmail.com>
2023-02-28 21:53:56 +0100

    wip - working on day01/ex02

--- day01/ex02/Vector.py
+++ day01/ex02/Vector.py
@@ -1,76 +1,44 @@
 class Vector:
     def __init__(self, param):
-        if type(param) == list:
-            self.values = param
-            self.size = len(param)
-        elif type(param) == int:
-            self.values = [float(elem) for elem in list(range(param))]
-            self.size = len(self.values)
-        elif type(param) == tuple:
-            self.values = [float(elem) for elem in list(range(param[0], param[1]))]
-            self.size = len(self.values)
+        if not isinstance(param, list) or not all(isinstance(elem, list) for elem in param)\
+            or not all(isinstance(number, float) for elem in param for number in elem):
+            raise TypeError("param should be a list of list of floats")
+        if not all(len(elem) == 1 for elem in param) and len(param) != 1:
+            raise ValueError("A vector is either a single line of floats or a single column of floats.")
+        self.values = param
+        self.shape = (len(param), len(param[0]) if param else 0)
 
-    def __add__(self, x):
-        if type(x) == int:
-            return Vector([elem + x for elem in self.values])
-        if type(x) == Vector:
-            if x.size != self.size:
-                print("Vectors should be in the same size")
-                return None
-            return Vector([self.values[i] + x.values[i] for i in range(self.size)])
-        return print("undefined")
-        
-    __radd__ = __add__
-
-    def __sub__(self, x):
-        if type(x) == int:
-            return Vector([elem - x for elem in self.values])
-        if type(x) == Vector:
-            if x.size != self.size:
-                print("Vectors should be in the same size")
-                return None
-            return Vector([self.values[i] - x.values[i] for i in range(self.size)])
-        return print("undefined")
-        
-    def __rsub__(self, x):
-        if type(x) == int:
-            return Vector([x - elem for elem in self.values])
-        if type(x) == Vector:
-            if x.size != self.size:
-                print("Vectors should be in the same size")
-                return None
-            return Vector([x.values[i] - self.values[i] for i in range(self.size)])
-        return print("undefined")
-        
-    def __truediv__(self, x):
-        if type(x) == int:
-            return Vector([elem / x for elem in self.values])
-        elif type(x) == Vector:
-            print("cannot devide two Vectors")
-            return None
-        return print("undefined")
-        
-
-    def __rtruediv__(self, x):
-        return print("undefined")
-        
-    def __mul__(self, x):
-        if type(x) == int:
-            return Vector([elem * x for elem in self.values])
-        elif type(x) == Vector:
-            if (x.size != self.size):
-                print("Vectors should be in the same size")
-                return None
-            return sum([x.values[i] * self.values[i] for i in range(self.size)])
-        return print("undefined")
+    def dot(self, other):
+        if not isinstance(other, Vector):
+            raise TypeError("other should be a Vector")
+        if self.shape != other.shape:
+            raise ValueError("Vectors should be in the same shape")
+        return sum(self.values[i][j] * other.values[i][j] for i in range(self.shape[0]) for j in range(self.shape[1]))
     
-    # def __rmul__(self, x):
-    #     if type(x) == int:
-    #         return Vector([elem * x for elem in self.values])
-    #     return print("undefined")
-    __rmul__ = __mul__
-    def __str__(self):  
-        return "elements : {}, size : {}".format(self.values, self.size)
+    def T(self):
+        return Vector([[self.values[j][i] for j in range(self.shape[0])] for i in range(self.shape[1])])
+   
+   
+if __name__ == "__main__":
+    v1 = Vector([[-5., -7.]])
+    print(v1.T().values)
+    v2 = Vector([[0., 7.]])
+    print(v2.T().values)
 
-    def __repr__(self):
-        return "Vector(values={}, size={})".format(self.values, self.size)
+
+    # print(v1.dot(v2))
+
+    v3 = Vector([[1.0, 3.0]])
+    print(v3.T().values)
+
+    v4 = Vector([[2.0], [4.0]])
+    print(v4.T().values)
+
+    v4 = Vector([])
+    print(v4.T().values)
+
+    # print(v3.dot(v4))
+
+    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+    # print(v1.dot(v2))
